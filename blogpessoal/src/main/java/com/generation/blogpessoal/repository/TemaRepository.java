6dacf05bfd5b4d42219c65f63bbaa8f10df69863
devgabrielnascimento <devgabrielnascimento@gmail.com>
2024-03-11 12:23:07 -0300

    Terminando métodos CRUD no PostagemController e criando estrutura MVC para tabela tema

---
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogpessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+
+}
